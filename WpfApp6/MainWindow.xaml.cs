8cb516d127e1c1c0fc43de3529576725fc15449e
CClemon40 <ggwp6900899@gmail.com>
2024-12-24 16:14:14 +0800

    Ver1

--- WpfApp6/MainWindow.xaml.cs
+++ WpfApp6/MainWindow.xaml.cs
@@ -16,9 +16,16 @@ namespace WpfApp6
     /// </summary>
     public partial class MainWindow : Window
     {
+        string defaultURL = "https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON";
         public MainWindow()
         {
             InitializeComponent();
+            UrlTextBox.Text = defaultURL;
+        }
+
+        private async void GetAQIButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentTextBox.Text = "資料抓取中...";
         }
     }
 }
